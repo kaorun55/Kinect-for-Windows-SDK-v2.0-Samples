7ed891bf64f628161d244ea88b9d5870898d6ddb
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-07-17 13:12:59 +0900

    C#(Windowsストア)を追加

---
+++ C#(WinRT)/_Template/KinectV2/KinectV2/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Kinect;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace KinectV2
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        KinectSensor kinect;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo( NavigationEventArgs e )
+        {
+            base.OnNavigatedTo( e );
+
+            try {
+                kinect = KinectSensor.GetDefault();
+                kinect.Open();
+            }
+            catch ( Exception ex ) {
+                MessageDialog dlg = new MessageDialog(ex.Message);
+                dlg.ShowAsync();
+            }
+        }
+
+        protected override void OnNavigatingFrom( NavigatingCancelEventArgs e )
+        {
+            base.OnNavigatingFrom( e );
+
+            if ( kinect != null ) {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+    }
+}
