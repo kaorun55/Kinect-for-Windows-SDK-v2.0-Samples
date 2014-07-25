174e05dcd65bf814b6834b2dfc59c4798f043577
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-07-25 11:32:18 +0900

    赤外線画像の取得を追加

---
+++ C#(dotNet)/05_Infrared/KinectV2-Infrared-01/KinectV2/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace KinectV2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        InfraredFrameReader infraredFrameReader;
+        FrameDescription infraredFrameDesc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded( object sender, RoutedEventArgs e )
+        {
+            try {
+                // Kinectを開く
+                kinect = KinectSensor.GetDefault();
+                if ( kinect == null ) {
+                    throw new Exception("Kinectを開けません");
+                }
+
+                kinect.Open();
+
+                // 赤外線画像の情報を取得する
+                infraredFrameDesc = kinect.InfraredFrameSource.FrameDescription;
+
+                // 赤外線リーダーを開く
+                infraredFrameReader = kinect.InfraredFrameSource.OpenReader();
+                infraredFrameReader.FrameArrived += infraredFrameReader_FrameArrived;
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show( ex.Message );
+                Close();
+            }
+        }
+
+        private void Window_Closing( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            // 終了処理
+            if ( infraredFrameReader != null ) {
+                infraredFrameReader.Dispose();
+                infraredFrameReader = null;
+            }
+
+            if ( kinect != null ) {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+
+        void infraredFrameReader_FrameArrived( object sender, InfraredFrameArrivedEventArgs e )
+        {
+            // カラーフレームを取得する
+            using ( var colorFrame = e.FrameReference.AcquireFrame() ) {
+                if ( colorFrame == null ) {
+                    return;
+                }
+
+                // 赤外線画像データを取得する
+                var infraredBuffer = new ushort[infraredFrameDesc.Width * infraredFrameDesc.Height];
+                colorFrame.CopyFrameDataToArray( infraredBuffer );
+
+                // ビットマップにする
+                ImageColor.Source = BitmapSource.Create( infraredFrameDesc.Width, infraredFrameDesc.Height, 96, 96,
+                    PixelFormats.Gray16, null, infraredBuffer, infraredFrameDesc.Width * (int)infraredFrameDesc.BytesPerPixel );
+            }
+        }
+    }
+}
---
+++ C++/05_Infrared/KinectV2-Infrared-01/KinectV2/main.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <sstream>
+
+#include <Kinect.h>
+#include <opencv2\opencv.hpp>
+
+// Visual Studio Professional以上を使う場合はCComPtrの利用を検討してください。
+#include "ComPtr.h"
+//#include <atlbase.h>
+
+// 次のように使います
+// ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+// 書籍での解説のためにマクロにしています。実際には展開した形で使うことを検討してください。
+#define ERROR_CHECK( ret )  \
+    if ( (ret) != S_OK ) {    \
+        std::stringstream ss;	\
+        ss << "failed " #ret " " << std::hex << ret << std::endl;			\
+        throw std::runtime_error( ss.str().c_str() );			\
+    }
+
+class KinectApp
+{
+private:
+
+    IKinectSensor* kinect = nullptr;
+    IInfraredFrameReader* infraredFrameReader = nullptr;
+    std::vector<UINT16> infraredBuffer;
+
+    int infraredWidth;
+    int infraredHeight;
+
+public:
+
+    // 初期化
+    void initialize()
+    {
+        // デフォルトのKinectを取得する
+        ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+
+        // Kinectを開く
+        ERROR_CHECK( kinect->Open() );
+
+        BOOLEAN isOpen = false;
+        ERROR_CHECK( kinect->get_IsOpen( &isOpen ) );
+        if ( !isOpen ){
+            throw std::runtime_error("Kinectが開けません");
+        }
+
+        // 赤外線画像リーダーを取得する
+        ComPtr<IInfraredFrameSource> infraredFrameSource;
+        ERROR_CHECK( kinect->get_InfraredFrameSource( &infraredFrameSource ) );
+        ERROR_CHECK( infraredFrameSource->OpenReader( &infraredFrameReader ) );
+
+        // 赤外線画像のサイズを取得する
+        ComPtr<IFrameDescription> infraredFrameDescription;
+        ERROR_CHECK( infraredFrameSource->get_FrameDescription( &infraredFrameDescription ) );
+        ERROR_CHECK( infraredFrameDescription->get_Width( &infraredWidth ) );
+        ERROR_CHECK( infraredFrameDescription->get_Height( &infraredHeight ) );
+
+        // バッファーを作成する
+        infraredBuffer.resize( infraredWidth * infraredHeight );
+    }
+
+    void run()
+    {
+        while ( 1 ) {
+            update();
+            draw();
+
+            auto key = cv::waitKey( 10 );
+            if ( key == 'q' ){
+                break;
+            }
+        }
+    }
+
+private:
+
+    // データの更新処理
+    void update()
+    {
+        updateInfrared();
+    }
+
+    void updateInfrared()
+    {
+        // フレームを取得する
+        ComPtr<IInfraredFrame> infraredFrame;
+        auto ret = infraredFrameReader->AcquireLatestFrame( &infraredFrame );
+        if ( ret == S_OK ){
+            // BGRAの形式でデータを取得する
+            ERROR_CHECK( infraredFrame->CopyFrameDataToArray( infraredBuffer.size(), &infraredBuffer[0] ) );
+
+            // フレームを解放する
+            // infraredFrame->Release();
+        }
+    }
+
+    void draw()
+    {
+        // カラーデータを表示する
+        cv::Mat colorImage( infraredHeight, infraredWidth, CV_16UC1, &infraredBuffer[0] );
+        cv::imshow( "Infrared Image", colorImage );
+    }
+};
+
+void main()
+{
+    try {
+        KinectApp app;
+        app.initialize();
+        app.run();
+    }
+    catch ( std::exception& ex ){
+        std::cout << ex.what() << std::endl;
+    }
+}
