59f74a44d16fec5e7415347d6d43e6f024e0e7c0
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-08-06 13:43:47 +0900

    Audioを追加

---
+++ C#(dotNet)/07_Audio/KinectV2-Audio-01/KinectV2/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectV2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        KinectSensor kinect;
+        AudioBeamFrameReader audioBeamFrameReader;
+        byte[] audioBuffer;
+
+        string fileName = "KinectAudio.wav";
+
+        WaveFile waveFile = new WaveFile();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded( object sender, RoutedEventArgs e )
+        {
+            try {
+                // Kinectを開く
+                kinect = KinectSensor.GetDefault();
+                kinect.Open();
+
+                // 音声バッファを作成する
+                audioBuffer = new byte[kinect.AudioSource.SubFrameLengthInBytes];
+
+                // 音声リーダーを開く
+                audioBeamFrameReader = kinect.AudioSource.OpenReader();
+                audioBeamFrameReader.FrameArrived += audioBeamFrameReader_FrameArrived;
+
+                audioBeamFrameReader.IsPaused = true;
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show( ex.Message );
+                Close();
+            }
+        }
+
+        void audioBeamFrameReader_FrameArrived( object sender, AudioBeamFrameArrivedEventArgs e )
+        {
+            using ( var audioFrame = e.FrameReference.AcquireBeamFrames() ) {
+                if ( audioFrame == null ) {
+                    return;
+                }
+
+                var subFrame = audioFrame[0].SubFrames[0];
+                subFrame.CopyFrameDataToArray( audioBuffer );
+
+                waveFile.Write( audioBuffer );
+
+                // (例)実際のデータは32bit IEEE floatデータなので変換する
+                float audioData1 = BitConverter.ToSingle( audioBuffer, 0 );
+                float audioData2 = BitConverter.ToSingle( audioBuffer, 4 );
+                float audioData3 = BitConverter.ToSingle( audioBuffer, 8 );
+            }
+        }
+
+        private void Window_Closing( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            if ( waveFile  != null ) {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+
+            if ( audioBeamFrameReader != null ) {
+                audioBeamFrameReader.Dispose();
+                audioBeamFrameReader = null;
+            }
+
+            if ( kinect != null ) {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+
+
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+            MediaWave.Source = null;
+            waveFile.Open( fileName );
+            audioBeamFrameReader.IsPaused = false;
+        }
+
+        private void Button_Click_1( object sender, RoutedEventArgs e )
+        {
+            audioBeamFrameReader.IsPaused = true;
+            waveFile.Close();
+        }
+
+        private void Button_Click_2( object sender, RoutedEventArgs e )
+        {
+            waveFile.Close();
+            audioBeamFrameReader.IsPaused = true;
+
+            MediaWave.Source = new Uri( string.Format( "./{0}", fileName ), UriKind.Relative );
+            MediaWave.Position = new TimeSpan();
+            MediaWave.Play();
+        }
+    }
+}
---
+++ C++/07_Audio/KinectV2-Audio-01/KinectV2/main.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#include <Kinect.h>
+#include <conio.h>
+
+// Visual Studio Professional以上を使う場合はCComPtrの利用を検討してください。
+#include "ComPtr.h"
+//#include <atlbase.h>
+
+#include "WaveFile.h"
+
+// 次のように使います
+// ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+// 書籍での解説のためにマクロにしています。実際には展開した形で使うことを検討してください。
+#define ERROR_CHECK( ret )  \
+    if ( (ret) != S_OK ) {    \
+        std::stringstream ss;	\
+        ss << "failed " #ret " " << std::hex << ret << std::endl;			\
+        throw std::runtime_error( ss.str().c_str() );			\
+        }
+
+class KinectApp
+{
+private:
+
+    IKinectSensor* kinect = nullptr;
+    IAudioBeamFrameReader* audioBeamFrameReader;
+
+    std::vector<float> audioBuffer;
+    UINT subFrameLengthInBytes = 0;
+
+    WaveFile audioFile;
+
+public:
+
+    // 初期化
+    void initialize()
+    {
+        // デフォルトのKinectを取得する
+        ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+
+        // Kinectを開く
+        ERROR_CHECK( kinect->Open() );
+
+        BOOLEAN isOpen = false;
+        ERROR_CHECK( kinect->get_IsOpen( &isOpen ) );
+        if ( !isOpen ){
+            throw std::runtime_error( "Kinectが開けません" );
+        }
+
+        initializeAudioSource();
+    }
+
+    void run()
+    {
+        std::cout << "キーを押すと終了します" << std::endl;
+
+        while ( 1 ) {
+            update();
+            draw();
+
+            if ( _kbhit() != 0 ){
+                break;
+            }
+        }
+    }
+
+private:
+
+    void initializeAudioSource()
+    {
+        // オーディオを開く
+        ComPtr<IAudioSource> audioSource;
+        ERROR_CHECK( kinect->get_AudioSource( &audioSource ) );
+
+        ERROR_CHECK( audioSource->OpenReader( &audioBeamFrameReader ) );
+        ERROR_CHECK( audioSource->get_SubFrameLengthInBytes( &subFrameLengthInBytes ) );
+
+        // バッファを作成する
+        audioBuffer.resize( subFrameLengthInBytes / sizeof( float ) );
+
+        audioFile.Open( "audio.wav" );
+    }
+    
+    // データの更新処理
+    void update()
+    {
+        updateAudioFrame();
+    }
+
+    // オーディオフレームの更新
+    void updateAudioFrame()
+    {
+        ComPtr<IAudioBeamFrameList> audioBeamFrameList;
+        auto ret = audioBeamFrameReader->AcquireLatestBeamFrames( &audioBeamFrameList );
+        if ( ret != S_OK ){
+            return;
+        }
+
+        ComPtr<IAudioBeamFrame> audioBeamFrame;
+        ERROR_CHECK( audioBeamFrameList->OpenAudioBeamFrame( 0, &audioBeamFrame ) );
+
+        ComPtr<IAudioBeamSubFrame> audioBeamSubFrame;
+        ERROR_CHECK( audioBeamFrame->GetSubFrame( 0, &audioBeamSubFrame ) );
+
+        audioBeamSubFrame->CopyFrameDataToArray( subFrameLengthInBytes, (BYTE*)&audioBuffer[0] );
+
+        audioFile.Write( &audioBuffer[0], subFrameLengthInBytes );
+    }
+
+    void draw()
+    {
+    }
+};
+
+void main()
+{
+    try {
+        KinectApp app;
+        app.initialize();
+        app.run();
+    }
+    catch ( std::exception& ex ){
+        std::cout << ex.what() << std::endl;
+    }
+}
